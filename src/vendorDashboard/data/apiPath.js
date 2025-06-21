d922bb782a52d4a2f34324551e38be809344e91a
D.chandu naik <139032331+chandu1267@users.noreply.github.com>
2025-06-22 00:24:24 +0530

    changed path to live api_url

--- src/vendorDashboard/data/apiPath.js
+++ src/vendorDashboard/data/apiPath.js
@@ -1,3 +1,4 @@
-export const API_URL = "http://localhost:8080"
-// export const API_URL = "https://hungerhub-backend-qp51.onrender.com"
+// export const API_URL = "http://localhost:8080"
+
+export const API_URL = "https://hungerhub-backend-qp51.onrender.com"
 
